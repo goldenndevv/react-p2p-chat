96bb6d703a54c9912930fc6cddc18aebb7bcef6e
goldenndevv <heracles.dev0628@gmail.com>
2016-02-28 22:03:10 +0100

    Initial setup for p2p communication

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello, there. Let's chat!")
+import Peer from 'simple-peer'
+
+const p1 = Peer({trickle: false, initiator: true})
+const p2 = Peer({trickle: false})
+
+p1.on('signal', (data) => {
+  console.log('p1 signal', data)
+  p2.signal(data)
+})
+p1.on('connect', () => console.log('p1 connected'))
+p1.on('data', (data) => console.log('p1 received', data))
+p1.on('error', (error) => console.error('p1 error', error))
+p1.on('close', () => console.log('p1 connection closed'))
+
+p2.on('signal', (data) => {
+  console.log('p2 signal', data)
+  p1.signal(data)
+})
+p2.on('connect', () => console.log('p2 connected'))
+p2.on('data', (data) => console.log('p2 received', data))
+p2.on('error', (error) => console.error('p2 error', error))
+p2.on('close', () => console.log('p2 connection closed'))
