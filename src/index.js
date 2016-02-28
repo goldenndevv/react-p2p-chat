f4dc9d097a21d81cdb0785eb1d3c5ef9f4042731
goldenndevv <heracles.dev0628@gmail.com>
2016-02-28 22:14:48 +0100

    p2p data exchange

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ p1.on('signal', (data) => {
   console.log('p1 signal', data)
   p2.signal(data)
 })
-p1.on('connect', () => console.log('p1 connected'))
-p1.on('data', (data) => console.log('p1 received', data))
+p1.on('connect', () => {
+  console.log('p1 connected')
+  p1.send('Hello, p2. How are you?')
+})
+p1.on('data', (data) => console.log('p1 received', data.toString('utf-8')))
 p1.on('error', (error) => console.error('p1 error', error))
 p1.on('close', () => console.log('p1 connection closed'))
 
@@ -17,6 +20,9 @@ p2.on('signal', (data) => {
   p1.signal(data)
 })
 p2.on('connect', () => console.log('p2 connected'))
-p2.on('data', (data) => console.log('p2 received', data))
+p2.on('data', (data) => {
+  console.log('p2 received', data.toString('utf-8'))
+  p2.send('Fine, thanks. How about you p1?')
+})
 p2.on('error', (error) => console.error('p2 error', error))
 p2.on('close', () => console.log('p2 connection closed'))
